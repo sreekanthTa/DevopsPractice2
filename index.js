c7cfd3d348dbd4cdfa2f499f5c782e995a0edcfc
sreekanthTa <sreekanthsreekantht970@gmail.com>
2025-12-28 11:09:28 +0530

    files

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Sample route
@@ -9,8 +8,5 @@ app.get('/', (req, res) => {
   res.send({message:'Hello, World!'});
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
 
 module.exports=app
